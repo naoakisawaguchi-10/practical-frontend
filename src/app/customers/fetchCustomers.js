5d34388adc525b41aaed69024b0089c7255289dd
naoaki.sawaguchi <naoaki.sawaguchi@gmail.com>
2025-06-07 20:48:00 +0900

    allcostomers

--- src/app/customers/fetchCustomers.js
+++ src/app/customers/fetchCustomers.js
@@ -1,6 +1,6 @@
 export default async function fetchCustomers() {
   const res = await fetch(
-    process.env.NEXT_PUBLIC_API_ENDPOINT + "/customers",
+    process.env.NEXT_PUBLIC_API_ENDPOINT + "/allcustomers",
     {
       cache: "no-cache",
     }
